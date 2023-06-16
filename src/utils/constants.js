02f82c296fd4928bd54b302bd6994e1a0b2ea258
CJ-King792 <136651559+CJ-King792@users.noreply.github.com>
2023-06-15 22:08:45 -0230

    Project Files

---
+++ src/utils/constants.js
@@ -0,0 +1,42 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'Newfie Songs', icon: <HomeIcon />, },
+  { name: 'Alan Doyle', icon: <CodeIcon />, },
+  { name: 'Jhawk23', icon: <CodeIcon />, },
+  { name: 'CBC NL', icon: <CodeIcon />, },
+  { name: 'NHL', icon: <CodeIcon />, },
+  { name: 'BC Newfie', icon: <MusicNoteIcon /> },
+  { name: 'Corner Brook', icon: <SchoolIcon />, },
+  { name: 'Stephenville NL', icon: <GraphicEqIcon />, },
+  { name: 'Newfie Lore', icon: <OndemandVideoIcon />, },
+  { name: 'NewfieCountryBoy', icon: <SportsEsportsIcon />, },
+  { name: 'Trailer Park Boys', icon: <LiveTvIcon />, },
+  { name: 'Knockout Hunting Adventure', icon: <FitnessCenterIcon />, },
+  { name: 'Republic of Doyle', icon: <CheckroomIcon />, },
+  { name: 'Newfie TikTok', icon: <FaceRetouchingNaturalIcon />, },
+  { name: '', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = 'JavaScript Mastery';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
